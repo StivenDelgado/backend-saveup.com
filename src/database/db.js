ee67e0dc66ffe074551efe772e09a822daaa6baa
Stiven Delgado <delgadostiven123@gmail.com>
2025-02-14 01:53:46 -0500

    estructura de archivos creada

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import pg from 'pg'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Client } = pg
+ 
+export const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: 5432,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false } // Habilita SSL
+})
+
+
