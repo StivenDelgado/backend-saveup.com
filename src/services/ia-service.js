278e1f86406ffa15a25fe53ff4dfd41cfe1e8744
StivenDelgado <delgadostiven123@gmail.com>
2025-03-01 19:56:15 -0500

    finalized ia

--- src/services/ia-service.js
+++ src/services/ia-service.js
@@ -21,7 +21,11 @@ class IAService {
                   maxOutputTokens: 100,
                 },
               });
-              const result = await chat.sendMessage(info.message);
+              const objetMessage = {
+                instructions: "Envía la respuesta que sea más claro, conciso y fácil de comprender, asegurándote de que el lenguaje sea sencillo y directo. Evita el uso de caracteres extraños o comandos de código, ya que el texto final se mostrará en un modal.",
+                message: info.message
+              }
+              const result = await chat.sendMessage(JSON.stringify(objetMessage));
               const response = await result.response.candidates[0].content.parts[0].text;	
             return {
                 success: true,
@@ -39,7 +43,11 @@ class IAService {
     async promptIA(info){
       try {
         const model = await GeminiClient.geminiModel();
-        const result = await model.generateContent(info.prompt);
+        const objetMessage = {
+          instructions: "Envía la respuesta que sea más claro, conciso y fácil de comprender, asegurándote de que el lenguaje sea sencillo y directo. Evita el uso de caracteres extraños o comandos de código, ya que el texto final se mostrará en un modal.",
+          message: info.prompt
+        }
+        const result = await model.generateContent(JSON.stringify(objetMessage));
         const response = result.response.candidates[0].content.parts[0].text;	
         return {
             success: true,
